04ffc07c9d5102e5e3bc7479e5f1f705397cbb1e
Yiyao Zhang <me@yiyao.ca>
2021-05-26 11:07:53 -0400

    Initial Commit

---
+++ ProjectSY2/ProjectSY2App.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectSY2App.swift
+//  ProjectSY2
+//
+//  Created by Yiyao Zhang on 2021-05-26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSY2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
